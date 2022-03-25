40182a1e3d95ea3e878bf29221ae678d9e9d7742
lauchness <lauchlanchisholm@gmail.com>
2022-03-25 15:06:26 -0300

    Adding logo as svg

---
+++ components/icons/BrightSize.tsx
@@ -0,0 +1,21 @@
+import React, { FunctionComponent } from "react";
+
+const BrightSize: FunctionComponent = () => {
+  return (
+    <svg
+      width="235"
+      height="235"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="37.5" cy="167.5" r="17.5" fill="#DDD0B0" />
+      <path
+        d="m117 90 47.631 82.5H69.369L117 90ZM207.763 34.631l-47.632 82.5-47.631-82.5h95.263Z"
+        fill="#437852"
+      />
+      <path stroke="#000" d="M20 199.5h195" />
+    </svg>
+  );
+};
+
+export default BrightSize;
---
+++ components/icons/index.ts
@@ -0,0 +1,1 @@
+export { default as BrightSize } from "./BrightSize";
