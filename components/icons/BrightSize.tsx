4ecbf6b9846c6eb2c24a90c63edda8b9ea2314ff
lauchness <lauchlanchisholm@gmail.com>
2022-03-25 16:34:22 -0300

    new bright size icon

--- components/icons/BrightSize.tsx
+++ components/icons/BrightSize.tsx
@@ -8,9 +8,9 @@ const BrightSize: FunctionComponent = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx="37.5" cy="167.5" r="17.5" fill="#DDD0B0" />
+      <circle cx="53.5" cy="93.5" r="17.5" fill="#FFCC49" />
       <path
-        d="m117 90 47.631 82.5H69.369L117 90ZM207.763 34.631l-47.632 82.5-47.631-82.5h95.263Z"
+        d="m85 114 47.631 82.5H37.369L85 114ZM175.763 58.631l-47.632 82.5-47.631-82.5h95.263Z"
         fill="#437852"
       />
       <path stroke="#000" d="M20 199.5h195" />
