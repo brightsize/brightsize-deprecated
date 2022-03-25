22bf04e68bf95fa650adcabdd65af73abb544bb7
lauchness <lauchlanchisholm@gmail.com>
2022-03-25 16:39:33 -0300

    purple and green

--- components/icons/BrightSize.tsx
+++ components/icons/BrightSize.tsx
@@ -9,9 +9,10 @@ const BrightSize: FunctionComponent = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <circle cx="53.5" cy="93.5" r="17.5" fill="#FFCC49" />
+      <path d="m85 114 47.631 82.5H37.369L85 114Z" fill="#437852" />
       <path
-        d="m85 114 47.631 82.5H37.369L85 114ZM175.763 58.631l-47.632 82.5-47.631-82.5h95.263Z"
-        fill="#437852"
+        d="m175.763 58.631-47.632 82.5-47.631-82.5h95.263Z"
+        fill="#95439C"
       />
       <path stroke="#000" d="M20 199.5h195" />
     </svg>
